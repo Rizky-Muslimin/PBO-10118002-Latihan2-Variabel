af52cfbde32f1ecab50a4acf8fe2d9df29210d3c
Rizky Muslimin <55826921+Rizky-Muslimin@users.noreply.github.com>
2019-09-26 15:23:20 +0700

    Add files via upload

---
+++ PBO1-10118002-Latihan2-Variabel/src/pbo1/pkg10118002/latihan2/variabel/PBO110118002Latihan2Variabel.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo1.pkg10118002.latihan2.variabel;
+
+/**
+ *
+ * @author ASUS
+ * Nama     : Rizky Muslimin
+ * Kelas    : IF1
+ * NIM      : 10118002
+ */
+public class PBO110118002Latihan2Variabel {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    //Deklarasi Variabel
+    int nilaiInt;
+    final double PHI = 3.14; //konstanta uppercase
+    boolean nilaiLogika;
+    char nilaiKarakter;
+    
+    //memberi nilai ke variabel
+    nilaiInt = 78;
+    nilaiLogika = false;
+    nilaiKarakter = 'D';
+    
+    //menampilkan hasil
+        System.out.println();
+        System.out.println("Isi variabel nilai = " + nilaiInt);
+        System.out.println("Isi variabel PHI =" + PHI);
+        System.out.println("Isi variabel logika =" + nilaiLogika);
+        System.out.println("Isi variabel karakter =" + nilaiKarakter);
+    }
+    
+}
